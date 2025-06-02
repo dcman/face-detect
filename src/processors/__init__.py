c09a0aa8ac651d6699772b75200b188250f1d08b
dcman <dcman@users.noreply.github.com>
2025-06-02 16:11:43 -0700

    Delete src/processors/__init__.py

--- src/processors/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Processors for face collection, clustering, and photo organization."""
-
-from .face_collector import FaceCollector
-from .face_clustering import FaceClustering
-from .photo_organizer import PhotoOrganizer
